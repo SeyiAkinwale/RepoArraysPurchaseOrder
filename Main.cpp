339a224ee77791edeca1798d44d9ecffbc50a072
Seyi <seyi.k.akinwale@gmail.com>
2022-11-22 12:06:52 -0800

    copied old code to modify

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,51 @@
+//Author: Seyi Akinwale
+//Purpose: Store information in database of purchase orders for plastics
+//Nov 22, 2022 
+
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	cout << "Hello World";
+//Prototype section: 3 functions
+//void getVals(int[], int, int&); //get 2 to 5 values from user
 
+//find the min, max and average from the user
+//void processVals(int[], int, int&, int&, double&);
+
+//output the original values, min, max and average
+//void showVals(int[], int, int&, int&, double&);
+
+//Main function: Tells user directions,
+//Calls the 3 program functions
+//Then allows user to repeat program
+int main() {
+
+	//Introduction and Directions for user:
+	cout << "*************************************************************\n";
+	//cout << "*Use this applet to compare scores to the average score     *\n";
+	//cout << "*You will start by entering the number of contestants.      *\n";
+	//cout << "*Then you will enter each contestant's score as an integer. *\n";
+	//cout << "*************************************************************\n";
+
+	//char repeatProgram;//user choice to run the program again
+	//const int maxCount = 5; //maximum possible values for the array
+
+	//do
+	//{
+	//	int numOfEntries = 0;
+	//	int arrayOfEntries[maxCount];
+
+	//	getVals(arrayOfEntries, maxCount, numOfEntries);
+	//	cout << endl << "numOfEntries" << numOfEntries << endl;
+	//	int highestVal = 0; //highest entry from user
+	//	int lowestVal = 0; //lowest entry from user
+	//	double average = 0; //average of user entries
+
+	//	processVals(arrayOfEntries, numOfEntries, highestVal, lowestVal, average);
+	//	showVals(arrayOfEntries, numOfEntries, highestVal, lowestVal, average);
+
+	//	cout << "Would you like to do it again? Y/N: ";
+	//	cin >> repeatProgram;
+	//	cout << endl << repeatProgram << endl;
+	//} while ((repeatProgram == 'Y') || (repeatProgram == 'y'));
 	return 0;
 }
